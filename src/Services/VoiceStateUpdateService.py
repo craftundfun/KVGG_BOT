5b5e9e08dc859107d0e75b3936b7db0c55c0acdc
bjarne <bjarneblu@gmail.com>
2023-11-26 17:19:41 +0100

    add a gender correct leave message and give the switch method the member to gain the correct name scheme

--- src/Services/VoiceStateUpdateService.py
+++ src/Services/VoiceStateUpdateService.py
@@ -165,7 +165,7 @@ class VoiceStateUpdateService:
                 dcUserDb['channel_id'] = voiceStateAfter.channel.id
 
                 self.__saveDiscordUser(dcUserDb)
-                self.waHelper.switchChannelFromOutstandingMessages(dcUserDb, voiceStateAfter.channel.name)
+                self.waHelper.switchChannelFromOutstandingMessages(dcUserDb, voiceStateAfter.channel.name, member)
 
                 await ChannelService.manageKneipe(voiceStateBefore.channel)
 
