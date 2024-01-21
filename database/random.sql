3f8ff7ad087371e4005ecfb7d4b8c0204ace738e
bjarne <bjarneblu@gmail.com>
2024-01-21 05:19:44 +0100

    some things

--- database/random.sql
+++ database/random.sql
@@ -2,13 +2,13 @@ USE beta;
 
 SELECT id FROM quest WHERE time_type = 'daily';
 
-DELETE FROM quest_discord_mapping WHERE id IN (SELECT id FROM quest WHERE time_type = 'daily')
+DELETE FROM quest_discord_mapping WHERE id IN (SELECT id FROM quest WHERE time_type = 'daily');
 
 USE testproject;
 
-SELECT q.*, qdm.* FROM quest q INNER JOIN quest_discord_mapping qdm ON q.id = qdm.quest_id ORDER BY qdm.time_created DESC
+SELECT q.*, qdm.* FROM quest q INNER JOIN quest_discord_mapping qdm ON q.id = qdm.quest_id ORDER BY qdm.time_created DESC;
 
-DROP TABLE counter
+DROP TABLE counter;
 
 SELECT rene_counter FROM discord WHERE username = "Bjarne";
 
