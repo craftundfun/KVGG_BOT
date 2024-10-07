1ac39ee16206000d4c9681854f4c7cbe3efb32be
bjarne <bjarneblu@gmail.com>
2024-10-07 21:12:24 +0200

    bugfix for external_game_id

--- src/Manager/BackgroundServiceManager.py
+++ src/Manager/BackgroundServiceManager.py
@@ -71,6 +71,7 @@ class BackgroundServices(commands.Cog):
 
         if dmManagerErrorCount >= 5:
             logger.debug("skipping dm-manager")
+
         try:
             await self.dmManager.sendMessages()
         except Exception as error:
